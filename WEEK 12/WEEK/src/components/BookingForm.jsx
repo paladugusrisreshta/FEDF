0253d6ae2e3307ee2a5392242cfda06f4982b06e
paladugusrisreshta <paladugusrisreshta@klh.edu.in>
2026-06-15 05:40:23 +0530

    Added folder

---
+++ WEEK 12/WEEK/src/components/BookingForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function BookingForm() {
+  return (
+    <section className="booking-form">
+      <h2>Book your ticket</h2>
+      <button type="button">Book Ticket</button>
+    </section>
+  );
+}
+
+export default BookingForm;
